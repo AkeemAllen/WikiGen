c04ba0bb3cb86012e1a61edd54902f0c26e174f9
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-03-13 23:57:24 -0400

    Added ability to map through and select a wiki

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<div class="mt-4">Welcome to WikiGen!</div>
+<script lang="ts">
+    import {wikis, selectedWiki} from "../store" 
+</script>
+
+<div class="mt-4">Select A Wiki</div>
+<div class="gird grid-cols-4">
+    {#each Object.entries($wikis) as [_, value]}
+        <button on:click={() => $selectedWiki = value} class="btn btn-primary">
+            {value.name}</button>
+    {/each}
+</div>
 
