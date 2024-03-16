0092cf239e0199a6282ce91678ab3fc8065f9580
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-03-16 01:07:06 -0400

    Checking for and creating wikis.json file on initial load

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,11 +1,15 @@
 /**@type {import('./$types').PageLoad} */
-import { BaseDirectory, readTextFile } from '@tauri-apps/api/fs';
+import { BaseDirectory, exists, readTextFile, writeTextFile } from '@tauri-apps/api/fs';
 import { wikis } from '../store';
 
 export const prerender = true;
 export const ssr = false;
 
 export async function load({params}) {
-    const contents = await readTextFile("data/wikis.json", {dir: BaseDirectory.AppData});
+    const wikiJsonExists = await exists("wikis.json", {dir: BaseDirectory.AppData});
+    if (!wikiJsonExists) {
+        await writeTextFile("wikis.json", JSON.stringify(wikis), {dir: BaseDirectory.AppData});
+    }
+    const contents = await readTextFile("wikis.json", {dir: BaseDirectory.AppData});
     wikis.set(JSON.parse(contents));
 }
