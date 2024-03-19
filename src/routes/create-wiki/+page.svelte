ca29ac3fa6aad5542a60e445f66761e01b68eef0
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-03-18 21:42:47 -0400

    Collecting more information for wiki settings

--- src/routes/create-wiki/+page.svelte
+++ src/routes/create-wiki/+page.svelte
@@ -3,14 +3,26 @@
   import { BaseDirectory, writeTextFile } from "@tauri-apps/api/fs";
   import { appDataDir, resourceDir } from "@tauri-apps/api/path";
   import { invoke } from "@tauri-apps/api/tauri";
-  import { wikis } from "../../store";
+  import { wikis, type WikiSettings } from "../../store";
 
   const toastStore = getToastStore();
 
   let wikiName = "";
+  let wikiCodeName = "";
   let wikiDescription = "";
+  let wikiAuthor = "";
+  let settings: WikiSettings = {
+    version_group: "",
+    matchup_generation: "current",
+    deployment_url: "",
+  };
 
-  const dirCreatedToast: ToastSettings = {
+  $: wikiCodeName = wikiName.toLowerCase().replaceAll(" ", "-");
+  $: siteUrl = `https://${wikiAuthor}.github.io/${wikiCodeName}`;
+  $: repoUrl = `https://github.com/${wikiAuthor}/${wikiCodeName}`;
+  $: siteName = wikiName;
+
+  const wikiCreatedToast: ToastSettings = {
     message: "Wiki Created",
     timeout: 5000,
     hoverable: true,
@@ -20,13 +32,14 @@
   async function createWiki() {
     const directory = await appDataDir();
     const resourceDirectory = await resourceDir();
-    $wikis[wikiName] = {
-      name: wikiName,
+    $wikis[wikiCodeName] = {
+      name: wikiCodeName,
       description: wikiDescription,
-      author: "author",
-      site_name: "site_name",
-      repo_url: "repo_url",
-      site_url: "site_url",
+      author: wikiAuthor,
+      site_name: siteName,
+      repo_url: repoUrl,
+      site_url: siteUrl,
+      settings: settings,
     };
     await writeTextFile("wikis.json", JSON.stringify($wikis), {
       dir: BaseDirectory.AppData,
@@ -34,12 +47,12 @@
     await invoke("create_wiki", {
       wikiName,
       wikiDescription,
-      wikiAuthor: "author",
-      siteName: "site_name",
+      wikiAuthor,
+      siteName,
       dir: directory,
       resourceDir: resourceDirectory,
     }).then(() => {
-      toastStore.trigger(dirCreatedToast);
+      toastStore.trigger(wikiCreatedToast);
     });
   }
 </script>
@@ -52,12 +65,56 @@
     bind:value={wikiName}
   />
   <input
+    disabled
+    class="p-1 border rounded-md disabled: bg-gray-100 text-gray-400"
+    placeholder="Code Name"
+    bind:value={wikiCodeName}
+  />
+  <textarea
     class="p-1 border rounded-md"
     placeholder="Wiki Description"
     bind:value={wikiDescription}
   />
+  <input
+    class="p-1 border rounded-md"
+    placeholder="Recommended: Your Github Username"
+    bind:value={wikiAuthor}
+  />
+  <input
+    class="p-1 border rounded-md"
+    placeholder="Can be left blank for now"
+    bind:value={settings.deployment_url}
+  />
+  <select
+    class="p-1 border rounded-md bg-white"
+    bind:value={settings.matchup_generation}
+  >
+    <option value="current">Current</option>
+    <option value="gen-1">Gen 1</option>
+    <option value="gen-2">Gen 2</option>
+  </select>
+  <input
+    disabled
+    class="p-1 border rounded-md disabled: bg-gray-100 text-gray-400"
+    placeholder="Repo URL"
+    bind:value={repoUrl}
+  />
+  <input
+    disabled
+    class="p-1 border rounded-md disabled: bg-gray-100 text-gray-400"
+    placeholder="Site URL"
+    bind:value={siteUrl}
+  />
+  <input
+    class="p-1 border rounded-md"
+    placeholder="Version Group"
+    bind:value={settings.version_group}
+  />
   <button
-    disabled={wikiDescription === "" || wikiName === ""}
+    disabled={wikiDescription === "" ||
+      wikiName === "" ||
+      wikiAuthor === "" ||
+      settings.version_group === ""}
     class="btn btn-md w-30 bg-blue-600"
     on:click={createWiki}>Create Wiki</button
   >
