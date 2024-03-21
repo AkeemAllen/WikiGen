507b3f5d4cc5bb6571d73b39f0272c4fac9d2239
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-03-21 16:17:38 -0400

    Removed unnecessary function from pokemon page

--- src/routes/pokemon/+page.svelte
+++ src/routes/pokemon/+page.svelte
@@ -1,27 +1,29 @@
 <script lang="ts">
-    import { TabGroup, Tab } from "@skeletonlabs/skeleton";
-    import {Command} from "@tauri-apps/api/shell";
-    import {invoke} from "@tauri-apps/api/tauri";
+  import { Tab, TabGroup } from "@skeletonlabs/skeleton";
 
-    let tabSet: number = 0
+  let tabSet: number = 0;
 
-    let pokemonName = ""
-    let message = ""
-    async function greet() { 
-        message = await invoke("greet", {pokemonName})
-    }
+  let pokemonName = "";
+  let message = "";
 </script>
 
 <TabGroup class="mt-4">
-    <Tab bind:group={tabSet} name="pokemon" value={0}>Pokemon</Tab>
-    <Tab bind:group={tabSet} name="prepare-pokemon-data" value={1}>Prepare Data</Tab>
-    <svelte:fragment slot="panel">
-        {#if tabSet === 0} 
-            <div>
-                <input id="greet-input" type="text" placeholder="Pokemon Name" bind:value="{pokemonName}" class="ml-2"/>
-                <button on:click="{greet}" class="btn">Search</button>
-                <p>{message}</p>
-            </div>
-        {/if}
-    </svelte:fragment>
+  <Tab bind:group={tabSet} name="pokemon" value={0}>Pokemon</Tab>
+  <Tab bind:group={tabSet} name="prepare-pokemon-data" value={1}
+    >Prepare Data</Tab
+  >
+  <svelte:fragment slot="panel">
+    {#if tabSet === 0}
+      <div>
+        <input
+          id="greet-input"
+          type="text"
+          placeholder="Pokemon Name"
+          bind:value={pokemonName}
+          class="ml-2"
+        />
+        <p>{message}</p>
+      </div>
+    {/if}
+  </svelte:fragment>
 </TabGroup>
