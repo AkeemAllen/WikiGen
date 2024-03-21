791897ceaa853b128eb2a8e499ba9723c7156547
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-03-20 23:27:00 -0400

    Improved Create Wiki page design

--- src/routes/create-wiki/+page.svelte
+++ src/routes/create-wiki/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import TextInput from "$lib/TextInput.svelte";
   import { getToastStore, type ToastSettings } from "@skeletonlabs/skeleton";
   import { BaseDirectory, writeTextFile } from "@tauri-apps/api/fs";
   import { appDataDir, resourceDir } from "@tauri-apps/api/path";
@@ -57,65 +58,106 @@
   }
 </script>
 
-<div class="flex flex-col gap-4 w-1/2">
-  <p class="mt-4 font-bold text-xl w-1/2">New Wiki</p>
-  <input
-    class="p-1 border rounded-md"
-    placeholder="Wiki Name"
-    bind:value={wikiName}
-  />
-  <input
-    disabled
-    class="p-1 border rounded-md disabled: bg-gray-100 text-gray-400"
-    placeholder="Code Name"
-    bind:value={wikiCodeName}
-  />
-  <textarea
-    class="p-1 border rounded-md"
-    placeholder="Wiki Description"
-    bind:value={wikiDescription}
-  />
-  <input
-    class="p-1 border rounded-md"
-    placeholder="Recommended: Your Github Username"
-    bind:value={wikiAuthor}
-  />
-  <input
-    class="p-1 border rounded-md"
-    placeholder="Can be left blank for now"
-    bind:value={settings.deployment_url}
-  />
-  <select
-    class="p-1 border rounded-md bg-white"
-    bind:value={settings.matchup_generation}
+<div class="grid grid-cols-3 p-4 gap-16 self-center">
+  <div>
+    <h2 class="text-base font-semibold leading-7 text-gray-900">New Wiki</h2>
+    <p class="mt-1 text-sm leading-6 text-gray-600">
+      Create a New Wiki to document all of the changes made to your Rom Hack.
+    </p>
+  </div>
+  <div
+    class="flex flex-col col-span-2 gap-y-5 card border rounded-lg p-5 w-[50rem]"
   >
-    <option value="current">Current</option>
-    <option value="gen-1">Gen 1</option>
-    <option value="gen-2">Gen 2</option>
-  </select>
-  <input
-    disabled
-    class="p-1 border rounded-md disabled: bg-gray-100 text-gray-400"
-    placeholder="Repo URL"
-    bind:value={repoUrl}
-  />
-  <input
-    disabled
-    class="p-1 border rounded-md disabled: bg-gray-100 text-gray-400"
-    placeholder="Site URL"
-    bind:value={siteUrl}
-  />
-  <input
-    class="p-1 border rounded-md"
-    placeholder="Version Group"
-    bind:value={settings.version_group}
-  />
-  <button
-    disabled={wikiDescription === "" ||
-      wikiName === "" ||
-      wikiAuthor === "" ||
-      settings.version_group === ""}
-    class="btn btn-md w-30 bg-blue-600"
-    on:click={createWiki}>Create Wiki</button
-  >
+    <div class="grid grid-cols-2 gap-16">
+      <TextInput
+        id="wikiName"
+        label="Wiki Name"
+        placeholder="Enter Wiki Name"
+        bind:value={wikiName}
+      />
+      <TextInput
+        id="code-name"
+        disabled={true}
+        label="Code Name"
+        placeholder="Code Name"
+        bind:value={wikiCodeName}
+      />
+    </div>
+    <div>
+      <label
+        for="wiki-description"
+        class="block text-sm font-medium leading-6 text-gray-900"
+        >Wiki Description</label
+      >
+      <textarea
+        id="wiki-description"
+        class="block w-full pl-2 mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-100 disabled:text-gray-400"
+        placeholder="Wiki Description"
+        bind:value={wikiDescription}
+      />
+    </div>
+    <div class="grid grid-cols-2 gap-16">
+      <TextInput
+        id="wiki-author"
+        label="Wiki Author"
+        placeholder="Recommended: Your Github Username"
+        bind:value={wikiAuthor}
+      />
+      <div>
+        <label for="matchup-generation" class="text-sm"
+          >Matchup Generation</label
+        >
+        <select
+          id="matchup-generation"
+          class="block w-full rounded-md border-0 mt-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+          bind:value={settings.matchup_generation}
+        >
+          <option value="current">Current</option>
+          <option value="gen-1">Gen 1</option>
+          <option value="gen-2">Gen 2</option>
+        </select>
+      </div>
+    </div>
+    <TextInput
+      id="deployment-url"
+      label="Deployment URL"
+      placeholder="Can be left blank for now"
+      bind:value={settings.deployment_url}
+    />
+    <div class="grid grid-cols-2 gap-16">
+      <TextInput
+        id="repo_url"
+        disabled={true}
+        label="Repo Url"
+        placeholder="Repo URL"
+        bind:value={repoUrl}
+      />
+      <TextInput
+        id="site_url"
+        disabled={true}
+        label="Site Url"
+        placeholder="Site URL"
+        bind:value={siteUrl}
+      />
+    </div>
+
+    <TextInput
+      id="version-group"
+      label="Version Group"
+      placeholder="Version Group"
+      bind:value={settings.version_group}
+    />
+
+    <button
+      disabled={wikiDescription === "" ||
+        wikiName === "" ||
+        wikiAuthor === "" ||
+        settings.version_group === ""}
+      class="rounded-md bg-indigo-600 w-32 px-3 py-2 text-sm font-semibold text-white
+      shadow-sm hover:bg-indigo-500 focus-visible:outline
+      focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
+      disabled:bg-indigo-400"
+      on:click={createWiki}>Create Wiki</button
+    >
+  </div>
 </div>
