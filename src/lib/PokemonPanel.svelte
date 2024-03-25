c76071fe5e9813a66b666d1fc7ed80ae29ebe5b5
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-03-24 21:18:42 -0400

    Added ability to read and edit pokemon details

--- src/lib/PokemonPanel.svelte
+++ src/lib/PokemonPanel.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
   import {
     Autocomplete,
+    Tab,
+    TabGroup,
     popup,
     type AutocompleteOption,
     type PopupSettings,
   } from "@skeletonlabs/skeleton";
-  import { pokemon, pokemonList, type PokemonData } from "../store/pokemon";
+  import {
+    PokemonTypes,
+    pokemon,
+    pokemonList,
+    type PokemonData,
+  } from "../store/pokemon";
+  import NumberInput from "./NumberInput.svelte";
+  import SelectInput from "./SelectInput.svelte";
+  import TextInput from "./TextInput.svelte";
+  import { capitalize } from "./utils";
 
   let pokemonName: string = "";
   let pokemonId: number = 0;
   let pokemonData: PokemonData = {} as PokemonData;
+
+  let tabSet: number = 0;
 
   const pokemonListOptions: AutocompleteOption<string | number>[] =
     $pokemonList.map(([name, id]) => ({ label: name, value: id }));
@@ -30,6 +43,8 @@
   function getPokemonData(): void {
     pokemonData = $pokemon.pokemon[pokemonId];
   }
+
+  $: console.log(pokemonData);
 </script>
 
 <div class="flex flex-row gap-7">
@@ -78,5 +93,139 @@
 </div>
 
 {#if pokemonData.id !== undefined}
-  <img src={pokemonData.sprite} alt={pokemonData.name} />
+  <img src={pokemonData.sprite} alt={pokemonData.name} width="100" />
+  <TabGroup class="px-4">
+    <Tab bind:group={tabSet} name="pokemon-details" value={0} class="text-sm"
+      >Pokemon Details</Tab
+    >
+    <Tab bind:group={tabSet} name="pokemon-moves" value={1} class="text-sm"
+      >Pokemon Moves</Tab
+    >
+    <svelte:fragment slot="panel">
+      <div class="scroll-smooth">
+        {#if tabSet === 0}
+          <div class="grid grid-cols-2 gap-x-10 gap-y-5 mt-10">
+            <SelectInput
+              id="pokemon-type-1"
+              bind:value={pokemonData.types[0]}
+              label="Type 1"
+              options={PokemonTypes.map((type) => ({
+                label: capitalize(type),
+                value: type,
+              }))}
+            />
+            <SelectInput
+              id="pokemon-type-2"
+              bind:value={pokemonData.types[1]}
+              label="Type 2"
+              options={PokemonTypes.map((type) => ({
+                label: capitalize(type),
+                value: type,
+              }))}
+            />
+            <SelectInput
+              id="pokemon-ability-1"
+              bind:value={pokemonData.abilities[0]}
+              label="Ability 1"
+              options={pokemonData.abilities.map((ability) => ({
+                label: capitalize(ability),
+                value: ability,
+              }))}
+            />
+            <SelectInput
+              id="pokemon-ability-2"
+              bind:value={pokemonData.abilities[1]}
+              label="Ability 2"
+              options={pokemonData.abilities.map((ability) => ({
+                label: capitalize(ability),
+                value: ability,
+              }))}
+            />
+          </div>
+          <div class="flex flex-row gap-x-10 mt-5">
+            <div class="w-44">
+              <SelectInput
+                id="evolution-method"
+                label="Evolution Method"
+                bind:value={pokemonData.evolution.method}
+                options={[
+                  { label: "No Change", value: "no_change" },
+                  { label: "Level Up", value: "level_up" },
+                  { label: "Other", value: "other" },
+                  { label: "Use Item", value: "item" },
+                ]}
+              />
+            </div>
+            {#if pokemonData.evolution.method === "item"}
+              <TextInput
+                id="evolution-item"
+                value={pokemonData.evolution?.item ?? ""}
+                label="Evolution Item"
+              />
+            {/if}
+            {#if pokemonData.evolution.method === "level_up"}
+              <NumberInput
+                id="evolution-level"
+                bind:value={pokemonData.evolution.level}
+                label="Evolution Level"
+              />
+            {/if}
+            {#if pokemonData.evolution.method === "other"}
+              <TextInput
+                id="evolution-other"
+                bind:value={pokemonData.evolution.other}
+                label="Evolution Other"
+              />
+            {/if}
+            {#if pokemonData.evolution.method !== "no_change"}
+              <div class="w-44">
+                <SelectInput
+                  id="evolution-to"
+                  label="Evoles To"
+                  bind:value={pokemonData.evolution.evolves_to}
+                  options={pokemonListOptions}
+                />
+              </div>
+            {/if}
+          </div>
+          <p class="text-lg mt-10">Stats</p>
+          <div class="grid grid-cols-2 gap-x-10 gap-y-5 mt-2 mb-2">
+            <NumberInput
+              id="pokemon-hp"
+              bind:value={pokemonData.stats.hp}
+              label="HP"
+            />
+            <NumberInput
+              id="pokemon-attack"
+              bind:value={pokemonData.stats.attack}
+              label="Attack"
+            />
+            <NumberInput
+              id="pokemon-defense"
+              bind:value={pokemonData.stats.defense}
+              label="Defense"
+            />
+            <NumberInput
+              id="pokemon-special-attack"
+              bind:value={pokemonData.stats.sp_attack}
+              label="Special Attack"
+            />
+            <NumberInput
+              id="pokemon-special-defense"
+              bind:value={pokemonData.stats.sp_defense}
+              label="Special Defense"
+            />
+            <NumberInput
+              id="pokemon-speed"
+              bind:value={pokemonData.stats.speed}
+              label="Speed"
+            />
+          </div>
+        {/if}
+      </div>
+      {#if tabSet === 1}
+        <p>Moves</p>
+      {/if}
+    </svelte:fragment>
+  </TabGroup>
 {/if}
