9c3f96df136b6807aa81f76cc1b0a03a21ad8d02
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-04-03 17:29:44 -0400

    Added Operation enum

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export enum Operation {
+  ADD = "add",
+  SHIFT = "shift",
+  DELETE = "delete",
+  REPLACE_MOVE = "replace_move",
+  REPLACE_BY_LEVEL = "replace_by_level",
+  SWAP_MOVES = "swap_moves",
+}
