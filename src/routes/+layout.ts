2208751d321b20195abfc1acb8c7b3653e611297
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-04-07 00:04:42 -0400

    Added check to ensure appDataDirectory's existence

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,11 +1,21 @@
 /**@type {import('./$types').PageLoad} */
-import { BaseDirectory, exists, readTextFile, writeTextFile } from '@tauri-apps/api/fs';
+import { BaseDirectory, createDir, exists, readTextFile, writeTextFile } from '@tauri-apps/api/fs';
 import { wikis } from '../store';
 
 export const prerender = true;
 export const ssr = false;
 
 export async function load({params}) {
+
+    // These lines ensure that the appData directory exists on initial application launch.
+    // Would prefer a more ideal way to do this, but this is the best I could come up with.
+    //
+    // The directory_existence folder just serves as a placeholder to ensure the appData directory is created.
+    const appDataDirectoryExists = await exists("directory_existence", {dir: BaseDirectory.AppData});
+    if (!appDataDirectoryExists) {
+        await createDir("directory_existence", {dir: BaseDirectory.AppData, recursive: true});
+    } 
+
     const wikiJsonExists = await exists("wikis.json", {dir: BaseDirectory.AppData});
     if (!wikiJsonExists) {
         await writeTextFile("wikis.json", JSON.stringify(wikis), {dir: BaseDirectory.AppData});
