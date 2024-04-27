d99029d5236850dc7891bcefe3e32caf64191ec8
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-04-27 02:24:06 -0400

    Added move structs module

---
+++ src-tauri/src/structs/move_structs.rs
@@ -0,0 +1,49 @@
+use std::{collections::HashMap, fmt};
+
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Moves {
+    pub moves: HashMap<String, MoveDetails>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MoveDetails {
+    pub accuracy: Option<u32>,
+    pub pp: u32,
+    pub power: Option<u32>,
+    #[serde(rename = "type")]
+    pub _type: String,
+    pub damage_class: String,
+    pub past_values: Vec<Value>,
+    pub machine_details: Option<Vec<MachineVersion>>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MachineVersion {
+    pub game_version: Option<String>,
+    pub technical_name: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MoveSetMove {
+    pub learn_method_detail: LearnMethodDetail,
+    pub power: Option<u32>,
+    pub _type: String,
+    pub accuracy: Option<u32>,
+    pub pp: u32,
+    pub damage_class: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum LearnMethodDetail {
+    LevelLearned(u32),
+    MachineName(String),
+}
+
+impl fmt::Display for LearnMethodDetail {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
