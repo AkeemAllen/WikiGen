f8b5979e1eb3d910d05f183b367025a386d822c8
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-04-29 02:00:38 -0400

    Moves pokemon wiki generation to tabgroup | Added toast confirmation for successful page generation

--- src/routes/wiki-generation/+page.svelte
+++ src/routes/wiki-generation/+page.svelte
@@ -2,40 +2,58 @@
   import Button from "$lib/components/Button.svelte";
 
   import NumberInput from "$lib/components/NumberInput.svelte";
+  import { Tab, TabGroup, getToastStore } from "@skeletonlabs/skeleton";
   import { invoke } from "@tauri-apps/api/tauri";
   import { selectedWiki } from "../../store";
+
+  const toastStore = getToastStore();
 
   let rangeStart: number = 1;
   let rangeEnd: number = 1;
   let loading: boolean = false;
+  let tabSet: number = 0;
 
   async function generatePokemonPagesInRange() {
-    console.log("Generating Pokemon from range");
+    loading = true;
     await invoke("generate_pokemon_pages_in_range", {
       rangeStart,
       rangeEnd,
       wikiName: $selectedWiki.name,
     }).then(() => {
-      console.log("Pokemon generated");
+      loading = false;
+      toastStore.trigger({
+        message: "Pokemon Pages generated",
+        timeout: 5000,
+        hoverable: true,
+        background: "variant-filled-success",
+      });
     });
   }
 </script>
 
-<div class="mt-6 ml-2">
-  <p>Wiki Generation</p>
-  <div class="flex gap-16">
-    <NumberInput id="range-start" label="Range Start" bind:value={rangeStart} />
-    <NumberInput id="range-end" label="Range End" bind:value={rangeEnd} />
-  </div>
-  <div class=" w-52 mt-4">
-    <Button
-      disabled={rangeStart === 0 ||
-        rangeEnd === 0 ||
-        rangeStart > rangeEnd ||
-        loading === true}
-      title="Generate Pokemon between range"
-      onClick={generatePokemonPagesInRange}
-      {loading}
-    />
-  </div>
-</div>
+<TabGroup class="mt-4 ml-2">
+  <Tab bind:group={tabSet} name="pokemon" value={0}>Pokemon</Tab>
+  <svelte:fragment slot="panel">
+    {#if tabSet === 0}
+      <div class="flex gap-16">
+        <NumberInput
+          id="range-start"
+          label="Range Start"
+          bind:value={rangeStart}
+        />
+        <NumberInput id="range-end" label="Range End" bind:value={rangeEnd} />
+      </div>
+      <div class=" w-40 mt-4">
+        <Button
+          disabled={rangeStart === 0 ||
+            rangeEnd === 0 ||
+            rangeStart > rangeEnd ||
+            loading === true}
+          title="Generate Pages"
+          onClick={generatePokemonPagesInRange}
+          {loading}
+        />
+      </div>
+    {/if}
+  </svelte:fragment>
+</TabGroup>
