f18df8071f0234a6022bb457059b3733aa187556
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-04-30 00:58:27 -0400

    Added functionality to create new routes

--- src-tauri/src/wiki_preparation/mod.rs
+++ src-tauri/src/wiki_preparation/mod.rs
@@ -1,3 +1,4 @@
 pub mod create_wiki;
+pub mod game_routes;
 pub mod prepare_data;
 pub mod yaml_declaration;
--- src/routes/game-routes/+page.svelte
+++ src/routes/game-routes/+page.svelte
@@ -1,1 +1,41 @@
-<div>Game Route</div>
+<script lang="ts">
+  import BaseModal from "$lib/components/BaseModal.svelte";
+  import Button from "$lib/components/Button.svelte";
+  import TextInput from "$lib/components/TextInput.svelte";
+  import { getToastStore } from "@skeletonlabs/skeleton";
+  import { invoke } from "@tauri-apps/api";
+  import { selectedWiki } from "../../store";
+
+  const toastStore = getToastStore();
+
+  let routeName: string = "";
+  let newRouteModalOpen: boolean = false;
+  let loading = false;
+
+  async function createNewRoute() {
+    loading = true;
+    await invoke("create_new_route", {
+      wikiName: $selectedWiki.name,
+      routeName,
+    }).then(() => {
+      loading = false;
+      toastStore.trigger({
+        message: "New Route Added",
+      });
+    });
+  }
+</script>
+
+<div class="w-40">
+  <Button title="Create New Route" onClick={() => (newRouteModalOpen = true)} />
+</div>
+<BaseModal bind:open={newRouteModalOpen}>
+  <TextInput bind:value={routeName} label="Route Name" />
+  <Button
+    title="Save New Route"
+    onClick={() => {
+      createNewRoute();
+      newRouteModalOpen = false;
+    }}
+  />
+</BaseModal>
