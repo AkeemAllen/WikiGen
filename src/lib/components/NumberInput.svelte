337ec1d4b6cac8846fc6d34ca974ce34c8e0f41f
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-04-30 16:25:22 -0400

    Added default min and max to NumberInput component

--- src/lib/components/NumberInput.svelte
+++ src/lib/components/NumberInput.svelte
@@ -2,6 +2,8 @@
   export let id = "";
   export let label = "";
   export let placeholder = "";
+  export let min = 0;
+  export let max = 100;
   export let value: number | null = 0;
   export let disabled = false;
 </script>
@@ -15,6 +17,8 @@
       {id}
       {disabled}
       type="number"
+      {min}
+      {max}
       {placeholder}
       class="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-100 disabled:text-gray-400"
       bind:value
