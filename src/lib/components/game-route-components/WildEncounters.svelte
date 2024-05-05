1a2573265f0011262569551d037bb6e988b82012
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-05-05 02:41:41 -0400

    Able to add wild encounters to routes

--- src/lib/components/game-route-components/WildEncounters.svelte
+++ src/lib/components/game-route-components/WildEncounters.svelte
@@ -1,71 +1,67 @@
 <script lang="ts">
-  import {
-    Autocomplete,
-    getToastStore,
-    popup,
-    type AutocompleteOption,
-  } from "@skeletonlabs/skeleton";
-  import { BaseDirectory, writeTextFile } from "@tauri-apps/api/fs";
-  import _ from "lodash";
-  import { selectedWiki } from "../../../store";
-  import { routes, type RouteProperties } from "../../../store/gameRoutes";
-  import { pokemonList, pokemon } from "../../../store/pokemon";
-  import Button from "../Button.svelte";
-  import NumberInput from "../NumberInput.svelte";
-  import SelectInput from "../SelectInput.svelte";
+import {
+  Autocomplete,
+  getToastStore,
+  popup,
+  type AutocompleteOption,
+} from "@skeletonlabs/skeleton";
+import { BaseDirectory, writeTextFile } from "@tauri-apps/api/fs";
+import _ from "lodash";
+import { selectedWiki } from "../../../store";
+import { routes, type RouteProperties } from "../../../store/gameRoutes";
+import { pokemonList, pokemon } from "../../../store/pokemon";
+import Button from "../Button.svelte";
+import NumberInput from "../NumberInput.svelte";
+import SelectInput from "../SelectInput.svelte";
 
-  export let routeName: string = "";
-  let pokemonName: string = "";
-  let encounterType: string = "grass-normal";
-  let encounterRate: number = 0;
-  let pokemonListOptions: AutocompleteOption<string | number>[] =
-    $pokemonList.map(([name, id]) => ({ label: name, value: id }));
+export let routeName: string = "";
+let pokemonName: string = "";
+let encounterType: string = "grass-normal";
+let encounterRate: number = 0;
+let pokemonListOptions: AutocompleteOption<string | number>[] =
+  $pokemonList.map(([name, id]) => ({ label: name, value: id }));
 
-  const encounterTypes = $routes.encounter_types.map((type) => ({
-    label: type,
-    value: type,
-  }));
+const encounterTypes = $routes.encounter_types.map((type) => ({
+  label: type,
+  value: type,
+}));
 
-  const toastStore = getToastStore();
-  function onPokemonNameSelected(
-    event: CustomEvent<AutocompleteOption<string | number>>,
-  ): void {
-    pokemonName = event.detail.label;
-  }
+const toastStore = getToastStore();
+function onPokemonNameSelected(
+  event: CustomEvent<AutocompleteOption<string | number>>,
+): void {
+  pokemonName = event.detail.label;
+}
 
-  async function writeRouteChangeToFile() {
-    await writeTextFile(
-      `${$selectedWiki.name}/data/routes.json`,
-      JSON.stringify($routes),
-      { dir: BaseDirectory.AppData },
-    ).then(() => {
-      toastStore.trigger({
-        message: "Encounter Added",
-        background: "variant-filled-success",
-      });
+async function addEncounter() {
+  let encounterTypesObjects = {
+    ...$routes.routes[routeName].wild_encounters,
+  };
+
+  $routes.routes[routeName].wild_encounters = {
+    ...$routes.routes[routeName].wild_encounters,
+    [encounterType]: [
+      ...(encounterTypesObjects[encounterType] ?? []),
+      {
+        id: $pokemonList.find(
+          ([name, _]) => name === pokemonName,
+        )?.[1] as number,
+        name: pokemonName,
+        encounter_rate: encounterRate,
+      },
+    ],
+  };
+  await writeTextFile(
+    `${$selectedWiki.name}/data/routes.json`,
+    JSON.stringify($routes),
+    { dir: BaseDirectory.AppData },
+  ).then(() => {
+    toastStore.trigger({
+      message: "Encounter Added",
+      background: "variant-filled-success",
     });
-  }
-
-  async function addEncounter() {
-    let encounterTypesObjects = {
-      ...$routes.routes[routeName].wild_encounters,
-    };
-
-    $routes.routes[routeName].wild_encounters = {
-      ...$routes.routes[routeName].wild_encounters,
-      [encounterType]: [
-        ...(encounterTypesObjects[encounterType] ?? []),
-        {
-          id: $pokemonList.find(
-            ([name, _]) => name === pokemonName,
-          )?.[1] as number,
-          name: pokemonName,
-          encounter_rate: encounterRate,
-        },
-      ],
-    };
-    await writeRouteChangeToFile();
-  }
+  });
+}
 </script>
 
 <div class="flex flex-row gap-x-5">
@@ -88,7 +84,7 @@
       id="pokemon-name"
       type="text"
       placeholder="Pokemon Name"
-      class="block w-full pl-2 mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-100 disabled:text-gray-400"
+      class="mt-2 block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:bg-gray-100 disabled:text-gray-400 sm:text-sm sm:leading-6"
       bind:value={pokemonName}
       use:popup={{
         event: "focus-click",
@@ -98,7 +94,7 @@
     />
     <div
       data-popup="popupAutoComplete"
-      class="card w-60 mt-2 overflow-y-auto bg-white rounded-sm"
+      class="card mt-2 w-60 overflow-y-auto rounded-sm bg-white"
       tabindex="-1"
     >
       <Autocomplete
@@ -106,12 +102,12 @@
         options={pokemonListOptions}
         limit={5}
         on:selection={onPokemonNameSelected}
-        class="bg-white w-full text-sm border rounded-md p-2"
+        class="w-full rounded-md border bg-white p-2 text-sm"
       />
     </div>
   </div>
   <NumberInput label="Encounter Rate" bind:value={encounterRate} />
-  <div class="w-32 mt-8">
+  <div class="mt-8 w-32">
     <Button
       title="Add Encounter"
       disabled={pokemonName === "" || encounterRate === 0}
@@ -120,27 +116,30 @@
   </div>
 </div>
 
-<div class="flex flex-col gap-y-5 mt-5">
+<div class="mt-5 flex flex-col gap-y-5">
   {#each Object.entries($routes.routes[routeName].wild_encounters ?? {}) as [_encounterType, encounters]}
     <div>
       <strong>
         {_.capitalize(_encounterType)} Encounters
       </strong>
-      <div class="grid grid-cols-6 gap-5 mt-2">
-      {#each encounters as encounter}
-        <div class="grid card shadow-md p-2 !bg-transparent">
-          <img src={$pokemon.pokemon[encounter.id].sprite} alt={encounter.name} class="m-0 justify-self-center"/>
-          <div class="border-2 w-full rounded-md">
-          <p class="text-center">
-        {_.capitalize(encounter.name)}
-          </p>
-          <p class="text-center">
-            {encounter.encounter_rate}%
-            </p>
+      <div class="mt-2 grid grid-cols-6 gap-5">
+        {#each encounters as encounter}
+          <div class="card grid !bg-transparent p-2 shadow-md">
+            <img
+              src={$pokemon.pokemon[encounter.id].sprite}
+              alt={encounter.name}
+              class="m-0 justify-self-center"
+            />
+            <div class="w-full rounded-md border-2">
+              <p class="text-center">
+                {_.capitalize(encounter.name)}
+              </p>
+              <p class="text-center">
+                {encounter.encounter_rate}%
+              </p>
+            </div>
           </div>
-        </div>
-      {/each}
-
+        {/each}
       </div>
       <div></div>
     </div>
