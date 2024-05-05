d47fb8443d2c9f5b4396639588136c73f4028b76
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-05-05 02:36:00 -0400

    Added Keydownhandler to textinput

--- src/lib/components/TextInput.svelte
+++ src/lib/components/TextInput.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-  export let id = "";
-  export let label = "";
-  export let placeholder = "";
-  export let value: string | null = "";
-  export let disabled = false;
-  export let inputHandler = () => {};
+import type { KeyboardEventHandler } from "svelte/elements";
+
+export let id = "";
+export let label = "";
+export let placeholder = "";
+export let value: string | null = "";
+export let disabled = false;
+export let inputHandler = () => {};
+export let onKeyDownHandler: KeyboardEventHandler<HTMLInputElement> = () => {};
 </script>
 
 <div>
@@ -15,13 +18,14 @@
   {/if}
   <div class="mt-2">
     <input
-      {id}
-      {disabled}
+      id={id}
+      disabled={disabled}
       type="text"
-      {placeholder}
+      placeholder={placeholder}
       on:input={inputHandler}
-      class="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-100 disabled:text-gray-400"
-      bind:value
+      on:keydown={onKeyDownHandler}
+      class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:bg-gray-100 disabled:text-gray-400 sm:text-sm sm:leading-6"
+      bind:value={value}
     />
   </div>
 </div>
