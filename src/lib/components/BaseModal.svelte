36f178a8ed176144137da56764a921cac37814f9
Akeem Allen <akeemallen@Akeems-Air.lan>
2024-05-10 00:09:12 -0400

    Added classes for base modal component

--- src/lib/components/BaseModal.svelte
+++ src/lib/components/BaseModal.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
-  export let open: boolean = false;
-  export let close: Function = () => (open = false);
+export let open: boolean = false;
+export let close: Function = () => (open = false);
+let className: string = "";
+export { className as class };
 </script>
 
 {#if open}
   <div
-    class="absolute top-0 bottom-0 left-0 right-0 h-screen grid justify-center items-center"
+    class="absolute bottom-0 left-0 right-0 top-0 grid h-screen items-center justify-center"
   >
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <!-- svelte-ignore a11y-no-static-element-interactions -->
     <div
       on:click={() => close()}
-      class="absolute top-0 bottom-0 left-0 right-0 h-screen bg-gray-900/90"
+      class="absolute bottom-0 left-0 right-0 top-0 h-screen bg-gray-900/90"
       style="z-index: 100;"
     />
     <div
-      class="relative flex flex-col gap-y-4 bg-white p-4 rounded-md"
+      class="{className} relative flex flex-col gap-y-4 rounded-md bg-white p-4"
       style="z-index: 1000;"
     >
       <slot />
