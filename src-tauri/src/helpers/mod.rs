1ad0c4a18b78f528ae3758bca2b18c25ba213e78
Akeem Allen <allenakeem8@gmail.com>
2024-05-18 09:09:35 -0400

    Moved copy_recursively function to helpers mod file

--- src-tauri/src/helpers/mod.rs
+++ src-tauri/src/helpers/mod.rs
@@ -1,4 +1,5 @@
-pub mod copy_recursively;
+use std::{fs, io, path::Path};
+
 pub mod matchups;
 pub mod mkdocs_process;
 
@@ -19,4 +20,19 @@ pub fn capitalize(s: &str) -> String {
         None => String::new(),
         Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
     }
+}
+
+/// Copy files from source to destination recursively.
+pub fn copy_recursively(source: impl AsRef<Path>, destination: impl AsRef<Path>) -> io::Result<()> {
+    fs::create_dir_all(&destination)?;
+    for entry in fs::read_dir(source)? {
+        let entry = entry?;
+        let filetype = entry.file_type()?;
+        if filetype.is_dir() {
+            copy_recursively(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
 }
