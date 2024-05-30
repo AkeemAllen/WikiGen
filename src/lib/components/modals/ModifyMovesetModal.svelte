89885d8d0e3274019ea4053e7967b9b7223ca534
Akeem Allen <allenakeem8@gmail.com>
2024-05-29 23:53:52 -0400

    Added hotkeys to pokemon moveset changes, close the modal, add row for movesetchange entry

--- src/lib/components/modals/ModifyMovesetModal.svelte
+++ src/lib/components/modals/ModifyMovesetModal.svelte
@@ -1,96 +1,100 @@
 <script lang="ts">
-  import { modifyLevelUpMoveSet } from "$lib/utils";
-  import {
-    Autocomplete,
-    popup,
-    type AutocompleteOption,
-  } from "@skeletonlabs/skeleton";
-  import { IconTrash } from "@tabler/icons-svelte";
-  import _ from "lodash";
-  import { moveList } from "../../../store/moves";
-  import { type PokemonDetails } from "../../../store/pokemon";
-  import { Operation, type MoveSetChange } from "../../../types";
-  import NumberInput from "../NumberInput.svelte";
-  import SelectInput from "../SelectInput.svelte";
+import { modifyLevelUpMoveSet } from "$lib/utils";
+import {
+  Autocomplete,
+  popup,
+  type AutocompleteOption,
+} from "@skeletonlabs/skeleton";
+import { IconTrash } from "@tabler/icons-svelte";
+import _ from "lodash";
+import { moveList } from "../../../store/moves";
+import { type PokemonDetails } from "../../../store/pokemon";
+import { Operation, type MoveSetChange } from "../../../types";
+import NumberInput from "../NumberInput.svelte";
+import SelectInput from "../SelectInput.svelte";
+import { shortcut } from "@svelte-put/shortcut";
 
-  export let open: boolean = false;
-  export let onClose: Function = () => (open = false);
-  export let pokemonDetails: PokemonDetails;
-  export let savePokemonChanges: Function;
+export let open: boolean = false;
+let moveSetChangeList: MoveSetChange[] = [];
+export let onClose: Function = () => {
+  open = false;
+  moveSetChangeList = [];
+};
+export let pokemonDetails: PokemonDetails;
+export let savePokemonChanges: Function;
 
-  const moveListOptions: AutocompleteOption<string>[] = $moveList.map(
-    (name) => ({ label: name, value: name }),
-  );
+const moveListOptions: AutocompleteOption<string>[] = $moveList.map((name) => ({
+  label: name,
+  value: name,
+}));
 
-  const pokemonMoveListOptions: AutocompleteOption<string>[] = Object.keys(
+const pokemonMoveListOptions: AutocompleteOption<string>[] = Object.keys(
+  pokemonDetails.moves,
+).map((name) => ({ label: name, value: name }));
+
+function onMoveNameSelected(
+  event: CustomEvent<AutocompleteOption<string>>,
+  field: "move" | "secondaryMove",
+  index: number,
+): void {
+  if (field === "move") {
+    moveSetChangeList[index].move = event.detail.value;
+  } else {
+    moveSetChangeList[index].secondaryMove = event.detail.value;
+  }
+}
+function addMoveSetChange() {
+  moveSetChangeList = [
+    ...moveSetChangeList,
+    {
+      operation: "add",
+      move: "",
+      level: 1,
+      secondaryMove: "",
+    },
+  ];
+}
+
+function removeMoveSetChange(index: number) {
+  moveSetChangeList = moveSetChangeList.filter((_, i) => i !== index);
+}
+
+function saveChanges() {
+  pokemonDetails.moves = modifyLevelUpMoveSet(
+    moveSetChangeList,
     pokemonDetails.moves,
-  ).map((name) => ({ label: name, value: name }));
-
-  let moveSetChangeList: MoveSetChange[] = [];
-
-  function onMoveNameSelected(
-    event: CustomEvent<AutocompleteOption<string>>,
-    field: "move" | "secondaryMove",
-    index: number,
-  ): void {
-    if (field === "move") {
-      moveSetChangeList[index].move = event.detail.value;
-    } else {
-      moveSetChangeList[index].secondaryMove = event.detail.value;
-    }
-  }
-  function addMoveSetChange() {
-    moveSetChangeList = [
-      ...moveSetChangeList,
-      {
-        operation: "add",
-        move: "",
-        level: 1,
-        secondaryMove: "",
-      },
-    ];
-  }
-
-  function removeMoveSetChange(index: number) {
-    moveSetChangeList = moveSetChangeList.filter((_, i) => i !== index);
-  }
-
-  function saveChanges() {
-    pokemonDetails.moves = modifyLevelUpMoveSet(
-      moveSetChangeList,
-      pokemonDetails.moves,
-    );
-    savePokemonChanges();
-    onClose();
-  }
+  );
+  savePokemonChanges();
+  onClose();
+}
 </script>
 
 {#if open}
   <div
-    class="absolute top-0 bottom-0 left-0 right-0 h-screen grid justify-center items-center"
+    class="absolute bottom-0 left-0 right-0 top-0 grid h-screen items-center justify-center"
   >
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <!-- svelte-ignore a11y-no-static-element-interactions -->
     <div
       on:click={() => onClose()}
-      class="absolute top-0 bottom-0 left-0 right-0 h-screen bg-gray-900/90"
+      class="absolute bottom-0 left-0 right-0 top-0 h-screen bg-gray-900/90"
       style="z-index: 100;"
     />
     <div
-      class="relative flex flex-col gap-y-4 bg-white p-4 rounded-md"
+      class="relative flex flex-col gap-y-4 rounded-md bg-white p-4"
       style="z-index: 1000;"
     >
       <button
         on:click={addMoveSetChange}
-        class=" rounded-md bg-indigo-600 w-full px-3 py-2 mt-2 text-sm font-semibold text-white
+        class=" mt-2 w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white
                    shadow-sm hover:bg-indigo-500 focus-visible:outline
                    focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
                    disabled:bg-indigo-400"
       >
         Add Moveset Change</button
       >
-      <div class="relative overflow-x-auto w-[50rem]">
-        <table class="w-full text-sm text-left rtl:text-right">
+      <div class="relative w-[50rem] overflow-x-auto">
+        <table class="w-full text-left text-sm rtl:text-right">
           <thead class="text-xs uppercase">
             <tr>
               <th class="px-6 py-3" scope="col">Operation</th>
@@ -102,8 +106,8 @@
           </thead>
           <tbody>
             {#each moveSetChangeList as row, index}
-              <tr class="bg-white border-b">
-                <td class="px-6 py-0 pb-1 w-52">
+              <tr class="border-b bg-white">
+                <td class="w-52 px-6 py-0 pb-1">
                   <SelectInput
                     bind:value={row.operation}
                     options={Object.values(Operation).map((value) => ({
@@ -117,7 +121,7 @@
                     id="move"
                     type="text"
                     placeholder="Move"
-                    class="block w-full pl-2 mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-100 disabled:text-gray-400"
+                    class="mt-2 block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:bg-gray-100 disabled:text-gray-400 sm:text-sm sm:leading-6"
                     bind:value={row.move}
                     use:popup={{
                       event: "focus-click",
@@ -127,7 +131,7 @@
                   />
                   <div
                     data-popup="movePopupAutoComplete{index}"
-                    class="fixed card w-60 mt-2 overflow-y-auto bg-white rounded-sm"
+                    class="card fixed mt-2 w-60 overflow-y-auto rounded-sm bg-white"
                     tabindex="-1"
                   >
                     <Autocomplete
@@ -151,7 +155,7 @@
                         ? pokemonMoveListOptions
                         : moveListOptions}
                       limit={5}
-                      class="bg-white w-full text-sm border rounded-md p-2 z-[100000]"
+                      class="z-[100000] w-full rounded-md border bg-white p-2 text-sm"
                     />
                   </div>
                 </td>
@@ -172,7 +176,7 @@
                       row.operation === Operation.ADD ||
                       row.operation === Operation.SHIFT ||
                       row.operation === Operation.REPLACE_BY_LEVEL}
-                    class="block w-full pl-2 mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-100 disabled:text-gray-400"
+                    class="mt-2 block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:bg-gray-100 disabled:text-gray-400 sm:text-sm sm:leading-6"
                     bind:value={row.secondaryMove}
                     use:popup={{
                       event: "focus-click",
@@ -182,7 +186,7 @@
                   />
                   <div
                     data-popup="secondaryMovePopupAutoComplete{index}"
-                    class="fixed card w-60 mt-2 overflow-y-auto bg-white rounded-sm"
+                    class="card fixed mt-2 w-60 overflow-y-auto rounded-sm bg-white"
                     tabindex="-1"
                   >
                     <Autocomplete
@@ -193,15 +197,15 @@
                         ? pokemonMoveListOptions
                         : moveListOptions}
                       limit={5}
-                      class="bg-white w-full text-sm border rounded-md p-2"
+                      class="w-full rounded-md border bg-white p-2 text-sm"
                     />
                   </div>
                 </td>
                 <td
-                  class="px-6 py-0 pb-1 hover:cursor-pointer hover:bg-gray-300 w-5 rounded-sm"
+                  class="w-5 rounded-sm px-6 py-0 pb-1 hover:cursor-pointer hover:bg-gray-300"
                   on:click={() => removeMoveSetChange(index)}
                 >
-                  <IconTrash size={18} class="text-gray-500 mt-4" />
+                  <IconTrash size={18} class="mt-4 text-gray-500" />
                 </td>
               </tr>
             {/each}
@@ -210,7 +214,8 @@
       </div>
       <button
         on:click={saveChanges}
-        class=" rounded-md bg-indigo-600 w-40 px-3 py-2 mt-2 text-sm font-semibold text-white
+        disabled={moveSetChangeList.length === 0}
+        class=" mt-2 w-40 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white
                shadow-sm hover:bg-indigo-500 focus-visible:outline
                focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
                disabled:bg-indigo-400"
@@ -220,3 +225,26 @@
     </div>
   </div>
 {/if}
+
+<svelte:window
+  use:shortcut={{
+    trigger: {
+      key: 'l',
+      modifier:"ctrl",
+      callback: () => addMoveSetChange(),
+    },
+  }}
+  use:shortcut={{
+    trigger: {
+      key: 'Escape',
+      callback: () => open = false,
+    },
+  }}
+  use:shortcut={{
+    trigger: {
+      key: 'Enter',
+      modifier: "meta",
+      callback: () => saveChanges(),
+    },
+  }}
+/>
