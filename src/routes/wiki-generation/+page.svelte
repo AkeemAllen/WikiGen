f65c47bdfb26fd78991ea3d42f51a340d1005f7c
Akeem Allen <allenakeem8@gmail.com>
2024-06-09 03:56:20 -0400

    Moved Wiki Generation for pokemon pages to Pokemon page as new tab replacing prepare data

--- src/routes/wiki-generation/+page.svelte
+++ src/routes/wiki-generation/+page.svelte
@@ -13,23 +13,6 @@ let rangeEnd: number = 1;
 let loading: boolean = false;
 let tabSet: number = 0;
 
-async function generatePokemonPagesInRange() {
-  loading = true;
-  await invoke("generate_pokemon_pages_from_range", {
-    rangeStart,
-    rangeEnd,
-    wikiName: $selectedWiki.name,
-  }).then(() => {
-    loading = false;
-    toastStore.trigger({
-      message: "Pokemon Pages generated",
-      timeout: 5000,
-      hoverable: true,
-      background: "variant-filled-success",
-    });
-  });
-}
-
 async function generateRoutePages() {
   loading = true;
   await invoke("generate_route_pages_with_handle", {
@@ -47,30 +30,9 @@ async function generateRoutePages() {
 </script>
 
 <TabGroup>
-  <Tab bind:group={tabSet} name="pokemon" value={0}>Pokemon</Tab>
-  <Tab bind:group={tabSet} name="routes" value={1}>Routes</Tab>
+  <Tab bind:group={tabSet} name="routes" value={0}>Routes</Tab>
   <svelte:fragment slot="panel">
     {#if tabSet === 0}
-      <div class="flex gap-16">
-        <NumberInput
-          id="range-start"
-          label="Range Start"
-          bind:value={rangeStart}
-        />
-        <NumberInput id="range-end" label="Range End" bind:value={rangeEnd} />
-      </div>
-      <Button
-        class=" mt-4 w-40"
-        disabled={rangeStart === 0 ||
-            rangeEnd === 0 ||
-            rangeStart > rangeEnd ||
-            loading === true}
-        title="Generate Pages"
-        onClick={generatePokemonPagesInRange}
-        loading={loading}
-      />
-    {/if}
-    {#if tabSet === 1}
       <Button
         class=" mt-4 w-40"
         title="Generate Pages"
