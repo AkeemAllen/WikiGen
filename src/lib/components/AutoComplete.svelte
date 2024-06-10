53be92c46dad753de8f9df7ff204313f59de1230
Akeem Allen <allenakeem8@gmail.com>
2024-06-09 23:44:22 -0400

    Updated autocomplete to take classname and showchevon props

--- src/lib/components/AutoComplete.svelte
+++ src/lib/components/AutoComplete.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 import { Autocomplete, popup } from "@skeletonlabs/skeleton";
+import { IconChevronDown } from "@tabler/icons-svelte";
 
 export let id: string = "";
 export let popupId: string = "";
 export let placeholder: string = "";
 export let label: string = "";
 export let value: string = "";
+export let showChevron: boolean = true;
 export let options: { label: string; value: string | number }[] = [];
 export let onSelection: (event: CustomEvent) => void;
 export let inputNode: HTMLInputElement = document.createElement("input");
@@ -13,26 +15,36 @@ export let onKeydown: (event: KeyboardEvent) => void = () => {};
 function setInputNode(node: HTMLElement, input: HTMLElement) {
   inputNode = node as HTMLInputElement;
 }
+
+let className: string = "";
+export { className as class };
 </script>
 
-<div class="w-60">
+<div class={className}>
   <label for={id} class="block text-sm font-medium leading-6 text-gray-900">
     {label}
   </label>
-  <input
-    id={id}
-    type="text"
-    placeholder={placeholder}
-    class="mt-2 block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:bg-gray-100 disabled:text-gray-400 sm:text-sm sm:leading-6"
-    bind:value={value}
-    use:popup={{
+  <div class="relative">
+    <input
+      id={id}
+      type="text"
+      placeholder={placeholder}
+      class="mt-2 block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:bg-gray-100 disabled:text-gray-400 sm:text-sm sm:leading-6"
+      bind:value={value}
+      use:popup={{
       event: "focus-click",
       target: popupId,
       placement: "bottom",
     }}
-    use:setInputNode={inputNode}
-    on:keydown={onKeydown}
-  />
+      use:setInputNode={inputNode}
+      on:keydown={onKeydown}
+    />
+    {#if showChevron}
+      <div class="absolute right-5 top-[10px] z-10 block">
+        <IconChevronDown size={18} class="text-gray-400" />
+      </div>
+    {/if}
+  </div>
   <div
     data-popup={popupId}
     class="card z-10 mt-2 w-60 overflow-y-auto rounded-sm bg-white"
