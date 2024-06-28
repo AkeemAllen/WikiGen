acca32c4a9c8e91b586c880765edde241730fb4e
Akeem Allen <allenakeem8@gmail.com>
2024-06-27 21:23:32 -0400

    Passing restProps into component

--- src/lib/components/TextInput.svelte
+++ src/lib/components/TextInput.svelte
@@ -28,6 +28,7 @@ export { className as class };
       on:keydown={onKeyDownHandler}
       class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:bg-gray-100 disabled:text-gray-400 sm:text-sm sm:leading-6"
       bind:value={value}
+      {...$$restProps}
     />
   </div>
 </div>
