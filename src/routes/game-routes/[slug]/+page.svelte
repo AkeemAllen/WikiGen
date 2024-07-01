e670c9df7fc065d7d600a1e3e2ed0ba877e2c596
Akeem Allen <allenakeem8@gmail.com>
2024-07-01 15:05:19 -0400

    Added back to Route page navigation

--- src/routes/game-routes/[slug]/+page.svelte
+++ src/routes/game-routes/[slug]/+page.svelte
@@ -2,12 +2,18 @@
 import TrainerEncounters from "$lib/components/game-route-components/TrainerEncounters.svelte";
 import WildEncounters from "$lib/components/game-route-components/WildEncounters.svelte";
 import { Tab, TabGroup } from "@skeletonlabs/skeleton";
+import { IconArrowLeft } from "@tabler/icons-svelte";
 
 export let data;
 let tabSet: number = 0;
 </script>
 
-<strong class="text-lg">{data.title}</strong>
+<strong class="text-l flex flex-row items-center gap-5">
+  <a href="/game-routes" class="hover:cursor-pointer">
+    <IconArrowLeft size={20} />
+  </a>
+  {data.title}</strong
+>
 <TabGroup class="mt-4">
   <Tab bind:group={tabSet} name="wild-encounters" value={0} class="text-sm"
     >Wild Encounters</Tab
