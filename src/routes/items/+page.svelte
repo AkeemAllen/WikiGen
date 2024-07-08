3ed961491650c1627fd124d7283460b49d7c0b86
Akeem Allen <allenakeem8@gmail.com>
2024-07-08 01:34:52 -0400

    Created new item button and modal

--- src/routes/items/+page.svelte
+++ src/routes/items/+page.svelte
@@ -2,6 +2,7 @@
 import _ from "lodash";
 import AutoComplete from "$lib/components/AutoComplete.svelte";
 import Button from "$lib/components/Button.svelte";
+import BaseModal from "$lib/components/BaseModal.svelte";
 import { itemsList, modifiedItems, type Item, items } from "../../store/items";
 import { modifiedNatures } from "../../store/natures";
 import { modifiedAbilities } from "../../store/abilities";
@@ -9,6 +10,7 @@ import { selectedWiki } from "../../store";
 import { BaseDirectory, writeTextFile } from "@tauri-apps/api/fs";
 import { getToastStore } from "@skeletonlabs/skeleton";
 import { invoke } from "@tauri-apps/api";
+import TextInput from "$lib/components/TextInput.svelte";
 
 const toastStore = getToastStore();
 
@@ -16,6 +18,10 @@ let itemName: string = "";
 let currentItemName: string = "";
 let itemDetails: Item = {} as Item;
 let originalItemDetails: Item = {} as Item;
+
+let newItemName: string = "";
+let newItemDetails: Item = {} as Item;
+let newItemModalOpen: boolean = false;
 
 let itemListOptions = $itemsList.map((item) => ({
   label: item,
@@ -107,7 +113,45 @@ async function saveItemChanges() {
     disabled={_.isEqual(itemDetails, originalItemDetails)}
     class="mt-2 w-32"
   />
+  <Button
+    title="Add New Item"
+    class="ml-auto mr-5 mt-2 w-32"
+    onClick={() => newItemModalOpen = true}
+  />
 </div>
+
+<BaseModal
+  class="w-[30rem]"
+  bind:open={newItemModalOpen}
+  close={() => {
+  newItemModalOpen = false;
+  newItemName = "";
+  newItemDetails.sprite = "";
+  newItemDetails.effect = "";
+}}
+>
+  <h2 class="text-lg font-medium leading-6 text-gray-900">Create New Item</h2>
+  <TextInput label="New Item Name" bind:value={newItemName} />
+  <TextInput label="Sprite" bind:value={newItemDetails.sprite} />
+  <div>
+    <label
+      for="effect"
+      class="block text-sm font-medium leading-6 text-gray-900">Effect</label
+    >
+    <div class="mt-2">
+      <textarea
+        id="effect"
+        bind:value={newItemDetails.effect}
+        class="block h-32 w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:bg-gray-100 disabled:text-gray-400 sm:text-sm sm:leading-6"
+      />
+    </div>
+  </div>
+  <Button
+    title="Create Item"
+    class="w-32"
+    disabled={newItemName === "" || newItemDetails.effect === "" || newItemDetails.sprite === ""}
+  />
+</BaseModal>
 
 {#if !_.isEmpty(itemDetails)}
   <p class="mt-4 text-lg">
