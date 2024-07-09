be93bf47717b2d5e66991b9140119900556c7e19
Akeem Allen <allenakeem8@gmail.com>
2024-07-08 22:23:39 -0400

    Cloning new item details value to items store

--- src/routes/items/+page.svelte
+++ src/routes/items/+page.svelte
@@ -30,7 +30,7 @@ $: itemListOptions = $itemsList.map((item) => ({
 }));
 
 async function saveItemChanges() {
-  updateItemModifications($modifiedItems, currentItemName, itemDetails);
+  updateItemModifications(currentItemName, itemDetails);
 
   $items[currentItemName] = itemDetails;
 
@@ -84,7 +84,7 @@ async function createNewItem() {
     { dir: BaseDirectory.AppData },
   );
 
-  $items[newItemName] = newItemDetails;
+  $items[newItemName] = _.clone(newItemDetails);
   itemsList.update((list) => {
     list.push(newItemName);
     return list;
@@ -101,6 +101,9 @@ async function createNewItem() {
       background: "variant-filled-success",
     });
     newItemModalOpen = false;
+    newItemName = "";
+    newItemDetails.sprite = "";
+    newItemDetails.effect = "";
     invoke("generate_item_page", { wikiName: $selectedWiki.name }).then(() => {
       toastStore.trigger({
         message: "Item page regenerated!",
@@ -157,6 +160,31 @@ async function deleteItem() {
 }
 </script>
 
+<BaseModal class="w-[30rem]" bind:open={newItemModalOpen}>
+  <h2 class="text-lg font-medium leading-6 text-gray-900">Create New Item</h2>
+  <TextInput label="New Item Name" bind:value={newItemName} />
+  <TextInput label="Sprite Url" bind:value={newItemDetails.sprite} />
+  <div>
+    <label
+      for="effect"
+      class="block text-sm font-medium leading-6 text-gray-900">Effect</label
+    >
+    <div class="mt-2">
+      <textarea
+        id="effect"
+        bind:value={newItemDetails.effect}
+        class="block h-32 w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:bg-gray-100 disabled:text-gray-400 sm:text-sm sm:leading-6"
+      />
+    </div>
+  </div>
+  <Button
+    title="Create Item"
+    class="w-32"
+    disabled={newItemName === "" || newItemDetails.effect === "" || newItemDetails.sprite === ""}
+    onClick={createNewItem}
+  />
+</BaseModal>
+
 <div class="flex flex-row gap-7">
   <AutoComplete
     bind:value={itemName}
@@ -196,40 +224,6 @@ async function deleteItem() {
     onClick={deleteItem}
   />
 </div>
-
-<BaseModal
-  class="w-[30rem]"
-  bind:open={newItemModalOpen}
-  close={() => {
-  newItemModalOpen = false;
-  newItemName = "";
-  newItemDetails.sprite = "";
-  newItemDetails.effect = "";
-}}
->
-  <h2 class="text-lg font-medium leading-6 text-gray-900">Create New Item</h2>
-  <TextInput label="New Item Name" bind:value={newItemName} />
-  <TextInput label="Sprite Url" bind:value={newItemDetails.sprite} />
-  <div>
-    <label
-      for="effect"
-      class="block text-sm font-medium leading-6 text-gray-900">Effect</label
-    >
-    <div class="mt-2">
-      <textarea
-        id="effect"
-        bind:value={newItemDetails.effect}
-        class="block h-32 w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:bg-gray-100 disabled:text-gray-400 sm:text-sm sm:leading-6"
-      />
-    </div>
-  </div>
-  <Button
-    title="Create Item"
-    class="w-32"
-    disabled={newItemName === "" || newItemDetails.effect === "" || newItemDetails.sprite === ""}
-    onClick={createNewItem}
-  />
-</BaseModal>
 
 {#if !_.isEmpty(itemDetails)}
   <p class="mt-4 text-lg">
