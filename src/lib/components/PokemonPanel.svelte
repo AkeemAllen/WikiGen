4fbc76da2469bcbd3de2b27d389d8b401ea0c390
Akeem Allen <allenakeem8@gmail.com>
2024-07-18 03:29:32 -0400

    Updated pokemon panel page to handle evolution change and ability updates

--- src/lib/components/PokemonPanel.svelte
+++ src/lib/components/PokemonPanel.svelte
@@ -7,7 +7,12 @@ import {
 } from "@tauri-apps/api/fs";
 import _ from "lodash";
 import { selectedWiki } from "../../store";
-import { pokemonList, type Pokemon, type Moveset } from "../../store/pokemon";
+import {
+  pokemonList,
+  type Pokemon,
+  type Moveset,
+  type EvolutionChange,
+} from "../../store/pokemon";
 import PokemonDetailsTab from "./PokemonDetailsTab.svelte";
 import PokemonMovesTab from "./PokemonMovesTab.svelte";
 import { invoke } from "@tauri-apps/api";
@@ -49,12 +54,17 @@ async function getPokemon() {
   }
 
   await $db
-    .select<Pokemon[]>("SELECT * FROM pokemon WHERE id = $1;", [
-      pokemonSearch[0],
-    ])
+    .select<Pokemon[]>(
+      `
+      SELECT *
+      FROM pokemon
+      WHERE pokemon.id = $1;`,
+      [pokemonSearch[0]],
+    )
     .then(async (res) => {
-      pokemon = res[0];
-      originalPokemonDetails = _.cloneDeep(pokemon);
+      // Using temp_pokemon to gather pokemon data before moveing it to pokemon variable so that
+      // the PokemonDetailsTab component doesn't load too early.
+      let temp_pokemon = res[0];
 
       // Gather moveset
       await $db
@@ -62,22 +72,91 @@ async function getPokemon() {
           `SELECT id, name, learn_method, level_learned FROM pokemon_movesets
             INNER JOIN moves on moves.id = pokemon_movesets.move
             WHERE pokemon = $1;`,
-          [pokemon.id],
+          [temp_pokemon.id],
         )
         .then((res) => {
           pokemonMoveset = res;
         })
         .catch((err) => {
-          console.log(err);
           toastStore.trigger({
             message: "Error loading Pokemon moveset!",
             background: "variant-filled-error",
           });
         });
 
+      // Gather abilities
+      await $db
+        .select<{ name: string }[]>(
+          `SELECT name FROM abilities
+            WHERE id = $1 OR id = $2;`,
+          [temp_pokemon.ability_1, temp_pokemon.ability_2],
+        )
+        .then((res) => {
+          if (res.length > 1) {
+            temp_pokemon.ability_2_name = _.capitalize(res[0].name);
+            temp_pokemon.ability_1_name = _.capitalize(res[1].name);
+            return;
+          }
+          temp_pokemon.ability_1_name = _.capitalize(res[0].name);
+          temp_pokemon.ability_2_name = "";
+        })
+        .catch((err) => {
+          toastStore.trigger({
+            message: "Error loading Pokemon abilities!",
+            background: "variant-filled-error",
+          });
+        });
+
+      // Gather evolution changes
+      await $db
+        .select<EvolutionChange[]>(
+          `SELECT
+            *,
+            pokemon.name as evolved_pokemon_name,
+            pokemon.id as evolved_pokemon_id
+          FROM evolution_changes
+          INNER JOIN pokemon ON pokemon.id = evolution_changes.evolved_pokemon
+          WHERE evolution_changes.id = $1;`,
+          [temp_pokemon.evolution_change],
+        )
+        .then((res) => {
+          console.log(res);
+          if (res.length === 0) {
+            temp_pokemon.evolution_change_object = {
+              id: 0,
+              method: "no_change",
+              evolved_pokemon: {
+                id: 0,
+                name: "",
+              },
+              item: null,
+              level: null,
+              other: null,
+            };
+          } else {
+            temp_pokemon.evolution_change_object = {
+              ...res[0],
+              evolved_pokemon: {
+                // @ts-ignore
+                id: res[0].evolved_pokemon_id,
+                // @ts-ignore
+                name: _.capitalize(res[0].evolved_pokemon_name),
+              },
+            };
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          console.log(temp_pokemon);
+          toastStore.trigger({
+            message: "Error loading Evolution change!",
+            background: "variant-filled-error",
+          });
+        });
+
       // Reading in image separately
       pokemonSprite = await readBinaryFile(
-        `${$selectedWiki.name}/dist/docs/img/pokemon/${pokemon.name}.png`,
+        `${$selectedWiki.name}/dist/docs/img/pokemon/${temp_pokemon.name}.png`,
         { dir: BaseDirectory.AppData },
       )
         .then((res) => {
@@ -91,13 +170,18 @@ async function getPokemon() {
           }
           return "Error loading image";
         });
+      return temp_pokemon;
+    })
+    .then((res) => {
+      pokemon = res;
+      originalPokemonDetails = _.cloneDeep(pokemon);
     })
     .catch((err) => {
-      console.log(err);
       toastStore.trigger({
         message: "Error loading Pokemon!",
         background: "variant-filled-error",
       });
+      return {} as Pokemon;
     });
 }
 
@@ -105,6 +189,20 @@ async function savePokemonChanges() {
   if (_.isEqual(pokemon, originalPokemonDetails)) {
     return;
   }
+
+  let evolutionChangeId = null;
+
+  if (
+    !_.isEqual(
+      pokemon.evolution_change_object,
+      originalPokemonDetails.evolution_change_object,
+    )
+  ) {
+    evolutionChangeId = await updateEvolutionChange();
+  }
+
+  console.log(pokemon);
+
   await $db
     .execute(
       `UPDATE pokemon SET
@@ -119,8 +217,9 @@ async function savePokemonChanges() {
           defense = $9,
           sp_attack = $10,
           sp_defense = $11,
-          speed = $12
-        WHERE id = $13;`,
+          speed = $12,
+          evolution_change = $13
+        WHERE id = $14;`,
       [
         pokemon.dex_number,
         pokemon.name,
@@ -134,6 +233,7 @@ async function savePokemonChanges() {
         pokemon.sp_attack,
         pokemon.sp_defense,
         pokemon.speed,
+        evolutionChangeId,
         pokemon.id,
       ],
     )
@@ -144,12 +244,142 @@ async function savePokemonChanges() {
         background: "variant-filled-success",
       });
     })
-    .catch(() => {
+    .catch((err) => {
+      console.log(err);
       toastStore.trigger({
         message: "Error saving pokemon changes!",
         background: "variant-filled-error",
       });
     });
+}
+
+async function updateEvolutionChange(): Promise<number | null> {
+  if (pokemon.evolution_change_object.method === "no_change") {
+    await $db
+      .execute(
+        `UPDATE pokemon SET
+          evolution_change = NULL
+        WHERE id = $1;`,
+        [pokemon.id],
+      )
+      .then(() => {
+        originalPokemonDetails.evolution_change_object = _.cloneDeep(
+          pokemon.evolution_change_object,
+        );
+        $db
+          .execute(`DELETE FROM evolution_changes WHERE id=$1`, [
+            pokemon.evolution_change,
+          ])
+          .then(() => {
+            pokemon.evolution_change = null;
+            pokemon.evolution_change_object = {
+              id: 0,
+              method: "no_change",
+              evolved_pokemon: {
+                id: 0,
+                name: "",
+              },
+              item: null,
+              level: null,
+              other: null,
+            };
+          })
+          .catch(() => {
+            console.log("Error deleting evolution change from table");
+          });
+        toastStore.trigger({
+          message: "Evolution change removed!",
+          background: "variant-filled-success",
+        });
+      })
+      .catch(() => {
+        toastStore.trigger({
+          message: "Error removing evolution change!",
+          background: "variant-filled-error",
+        });
+      });
+    return null;
+  }
+
+  if (pokemon.evolution_change === null) {
+    let evolved_pokemon_id = null;
+    if (pokemon.evolution_change_object.evolved_pokemon.id !== 0) {
+      evolved_pokemon_id = pokemon.evolution_change_object.evolved_pokemon.id;
+    }
+    let evolution_change_id = null;
+    await $db
+      .execute(
+        `INSERT INTO evolution_changes (
+            method,
+            level,
+            item,
+            other,
+            evolved_pokemon
+          ) VALUES ($1, $2, $3, $4, $5)`,
+        [
+          pokemon.evolution_change_object.method,
+          pokemon.evolution_change_object.level,
+          pokemon.evolution_change_object.item,
+          pokemon.evolution_change_object.other,
+          evolved_pokemon_id,
+        ],
+      )
+      .then((res) => {
+        pokemon.evolution_change = res.lastInsertId;
+        pokemon.evolution_change_object.id = res.lastInsertId;
+        evolution_change_id = res.lastInsertId;
+        originalPokemonDetails = _.cloneDeep(pokemon);
+        toastStore.trigger({
+          message: "Evolution Change Added",
+          background: "variant-filled-success",
+        });
+        return res.lastInsertId;
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("Evolution Change", pokemon.evolution_change);
+        console.log("Evolution Change Object", pokemon.evolution_change_object);
+      });
+    return evolution_change_id;
+  }
+
+  if (pokemon.evolution_change_object.method === "item") {
+    pokemon.evolution_change_object.level = null;
+    pokemon.evolution_change_object.other = null;
+  } else if (pokemon.evolution_change_object.method === "level_up") {
+    pokemon.evolution_change_object.item = null;
+    pokemon.evolution_change_object.other = null;
+  } else if (pokemon.evolution_change_object.method === "other") {
+    pokemon.evolution_change_object.item = null;
+    pokemon.evolution_change_object.level = null;
+  }
+
+  await $db
+    .execute(
+      `UPDATE evolution_changes SET
+        method=$1,
+        level=$2,
+        item=$3,
+        other=$4,
+        evolved_pokemon=$5
+      WHERE id=$6`,
+      [
+        pokemon.evolution_change_object.method,
+        pokemon.evolution_change_object.level,
+        pokemon.evolution_change_object.item,
+        pokemon.evolution_change_object.other,
+        pokemon.evolution_change_object.evolved_pokemon.id,
+        pokemon.evolution_change,
+      ],
+    )
+    .then(() => {
+      toastStore.trigger({
+        message: "Evolution Change Updated",
+        background: "variant-filled-success",
+      });
+      originalPokemonDetails = _.cloneDeep(pokemon);
+    });
+  return pokemon.evolution_change;
 }
 
 // function nextPokemon() {
