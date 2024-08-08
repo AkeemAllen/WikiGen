20bd1889113849b44f5d021a26852ec676e1e4f8
Akeem Allen <allenakeem8@gmail.com>
2024-08-08 16:43:34 -0400

    Changed dexNumbers to pokemonIds

--- src/routes/pokemon/+page.svelte
+++ src/routes/pokemon/+page.svelte
@@ -1,45 +1,49 @@
 <script lang="ts">
-import Button from "$lib/components/Button.svelte";
-import NumberInput from "$lib/components/NumberInput.svelte";
-import PokemonPanel from "$lib/components/PokemonPanel.svelte";
-import { Tab, TabGroup, getToastStore } from "@skeletonlabs/skeleton";
-import { invoke } from "@tauri-apps/api/tauri";
-import { selectedWiki } from "../../store";
-import NewPokemonPanel from "$lib/components/NewPokemonPanel.svelte";
+  import Button from "$lib/components/Button.svelte";
+  import NumberInput from "$lib/components/NumberInput.svelte";
+  import PokemonPanel from "$lib/components/PokemonPanel.svelte";
+  import { Tab, TabGroup, getToastStore } from "@skeletonlabs/skeleton";
+  import { invoke } from "@tauri-apps/api/tauri";
+  import { selectedWiki } from "../../store";
+  import NewPokemonPanel from "$lib/components/NewPokemonPanel.svelte";
 
-const toastStore = getToastStore();
+  const toastStore = getToastStore();
 
-let tabSet: number = 0;
-let rangeStart: number = 0;
-let rangeEnd: number = 0;
-let loading: boolean = false;
+  let tabSet: number = 0;
+  let rangeStart: number = 0;
+  let rangeEnd: number = 0;
+  let loading: boolean = false;
 
-async function generatePokemonPagesInRange() {
-  loading = true;
-  let dexNumbers: number[] = [];
-  for (let i = rangeStart; i <= rangeEnd; i++) {
-    dexNumbers.push(i);
-  }
-  await invoke("generate_pokemon_pages_from_list", {
-    dexNumbers: dexNumbers,
-    wikiName: $selectedWiki.name,
-  }).then(() => {
-    loading = false;
-    toastStore.trigger({
-      message: "Pokemon Pages generated",
-      timeout: 5000,
-      hoverable: true,
-      background: "variant-filled-success",
+  async function generatePokemonPagesInRange() {
+    loading = true;
+    let pokemonIds: number[] = [];
+    for (let i = rangeStart; i <= rangeEnd; i++) {
+      pokemonIds.push(i);
+    }
+    await invoke("generate_pokemon_pages_from_list", {
+      pokemonIds: pokemonIds,
+      wikiName: $selectedWiki.name,
+    }).then(() => {
+      loading = false;
+      toastStore.trigger({
+        message: "Pokemon Pages generated",
+        timeout: 5000,
+        hoverable: true,
+        background: "variant-filled-success",
+      });
     });
-  });
-}
+  }
 </script>
 
 <TabGroup>
-  <Tab bind:group={tabSet} name="pokemon" value={0} class="text-sm">Pokemon</Tab>
-  <Tab bind:group={tabSet} name="prepare-pokemon-data" value={1} class="text-sm">Generation</Tab
+  <Tab bind:group={tabSet} name="pokemon" value={0} class="text-sm">Pokemon</Tab
   >
-  <Tab bind:group={tabSet} name="new-pokemon" value={2} class="text-sm">Create New Pokemon</Tab>
+  <Tab bind:group={tabSet} name="prepare-pokemon-data" value={1} class="text-sm"
+    >Generation</Tab
+  >
+  <Tab bind:group={tabSet} name="new-pokemon" value={2} class="text-sm"
+    >Create New Pokemon</Tab
+  >
   <svelte:fragment slot="panel">
     {#if tabSet === 0}
       <PokemonPanel />
@@ -63,12 +67,12 @@ async function generatePokemonPagesInRange() {
       <Button
         class=" mt-4 w-40"
         disabled={rangeStart === 0 ||
-                    rangeEnd === 0 ||
-                    rangeStart > rangeEnd ||
-                    loading === true}
+          rangeEnd === 0 ||
+          rangeStart > rangeEnd ||
+          loading === true}
         title="Generate Pages"
         onClick={generatePokemonPagesInRange}
-        loading={loading}
+        {loading}
       />
     {/if}
     {#if tabSet === 2}
