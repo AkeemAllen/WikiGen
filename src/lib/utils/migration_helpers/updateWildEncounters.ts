921a3b40f6b00e33e514d1b0495a5ba77c418072
Akeem Allen <allenakeem8@gmail.com>
2024-08-23 12:47:07 -0400

    Added functionality to transition route wild encounters to new format

---
+++ src/lib/utils/migration_helpers/updateWildEncounters.ts
@@ -0,0 +1,26 @@
+import { get } from "svelte/store";
+import type { Routes } from "../../../store/gameRoutes";
+
+export default function updateWildEncounters(routes: Routes): Routes {
+  let updatedRoutes = routes;
+
+  for (const [routeName, routeProperties] of Object.entries(
+    updatedRoutes.routes,
+  )) {
+    for (const [encounterArea, wildEncounters] of Object.entries(
+      routeProperties.wild_encounters,
+    )) {
+      for (const [index, encounter] of wildEncounters.entries()) {
+        updatedRoutes.routes[routeName].wild_encounters[encounterArea][index] =
+          {
+            ...encounter,
+            route: routeName,
+            special_note: "",
+            encounter_type: encounterArea,
+          };
+      }
+    }
+  }
+
+  return updatedRoutes;
+}
