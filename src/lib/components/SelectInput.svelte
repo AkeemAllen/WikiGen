65548d9a3b65ee52097bc4dae652e25190f37798
Akeem Allen <allenakeem8@gmail.com>
2024-08-22 20:14:37 -0400

    Added restprops to selectinput and added disabled css

--- src/lib/components/SelectInput.svelte
+++ src/lib/components/SelectInput.svelte
@@ -20,9 +20,10 @@
     <select
       {id}
       style={"-webkit-appearance: none;"}
-      class=" block w-full rounded-md border-0 pl-2 py-1.5 mt-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      class=" block w-full rounded-md border-0 pl-2 py-1.5 mt-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-100 disabled:text-gray-400"
       bind:value
       on:change={onChange}
+      {...$$restProps}
     >
       {#each options as option}
         <option value={option.value}>{option.label}</option>
