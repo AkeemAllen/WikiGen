b95ed528f3f3b6a9e60bcaadf940250694bdec32
Akeem Allen <allenakeem8@gmail.com>
2024-08-29 04:09:45 -0400

    Added ability to upload route images and display them on routes

--- src/routes/game-routes/[slug]/+page.svelte
+++ src/routes/game-routes/[slug]/+page.svelte
@@ -2,18 +2,95 @@
   import TrainerEncounters from "$lib/components/game-route-components/TrainerEncounters.svelte";
   import WildEncounters from "$lib/components/game-route-components/WildEncounters.svelte";
   import SelectInput from "$lib/components/SelectInput.svelte";
+  import Button from "$lib/components/Button.svelte";
   import { getToastStore, Tab, TabGroup } from "@skeletonlabs/skeleton";
   import { IconArrowLeft } from "@tabler/icons-svelte";
   import { routes } from "../../../store/gameRoutes";
   import { selectedWiki } from "../../../store";
-  import { BaseDirectory, writeTextFile } from "@tauri-apps/api/fs";
+  import {
+    BaseDirectory,
+    createDir,
+    exists,
+    readBinaryFile,
+    writeBinaryFile,
+    writeTextFile,
+  } from "@tauri-apps/api/fs";
   import { invoke } from "@tauri-apps/api";
+  import { onMount } from "svelte";
+  import { base64ToArray } from "$lib/utils";
 
   const toastStore = getToastStore();
   export let data;
   let tabSet: number = 0;
+  let newRouteImage: string = "";
 
-  async function saveRenderChange() {
+  onMount(async () => {
+    newRouteImage = await readBinaryFile(
+      `${$selectedWiki.name}/dist/docs/img/routes/${data.title}.png`,
+      { dir: BaseDirectory.AppData },
+    )
+      .then((res) => {
+        const blob = new Blob([res], { type: "image/png" });
+        return URL.createObjectURL(blob);
+      })
+      .catch(() => {
+        return "";
+      });
+  });
+
+  function onImageUpload(e: any) {
+    let file = e.target.files[0];
+    let reader = new FileReader();
+    reader.onloadend = (e) => {
+      let base64 = e.target?.result as string;
+      if (!base64.includes("data:image/png;base64,")) {
+        toastStore.trigger({
+          message: "Invalid image format!",
+          background: "variant-filled-error",
+        });
+        return;
+      }
+      newRouteImage = e.target?.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  async function saveChanges() {
+    const imageBytes = base64ToArray(
+      newRouteImage.replace("data:image/png;base64,", ""),
+      "image/png",
+    );
+
+    const routeImagesDirectoryExists = await exists(
+      `${$selectedWiki.name}/dist/docs/img/routes`,
+      {
+        dir: BaseDirectory.AppData,
+      },
+    );
+
+    if (!routeImagesDirectoryExists) {
+      await createDir(`${$selectedWiki.name}/dist/docs/img/routes`, {
+        dir: BaseDirectory.AppData,
+        recursive: true,
+      });
+    }
+
+    await writeBinaryFile(
+      `${$selectedWiki.name}/dist/docs/img/routes/${data.title}.png`,
+      imageBytes,
+      { dir: BaseDirectory.AppData },
+    )
+      .then(() => {
+        toastStore.trigger({
+          message: "Route Image Saved",
+          timeout: 3000,
+          background: "variant-filled-success",
+        });
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+
     await writeTextFile(
       `${$selectedWiki.name}/data/routes.json`,
       JSON.stringify($routes),
@@ -25,7 +102,7 @@
       })
         .then(() => {
           toastStore.trigger({
-            message: "Render Value Updated",
+            message: "Route Property Changes Saved",
             timeout: 3000,
             background: "variant-filled-success",
           });
@@ -61,6 +138,24 @@
       <TrainerEncounters routeName={data.title} />
     {/if}
     {#if tabSet === 2}
+      <Button title="Save Changes" onClick={saveChanges} class="w-32" />
+      <div class="mb-4 mt-4">
+        <label
+          for="sprite-image"
+          class="block text-sm font-medium leading-6 text-gray-900"
+          >Route Image</label
+        >
+        {#if newRouteImage !== ""}
+          <img src={newRouteImage} alt="Route" />
+        {/if}
+        <input
+          id="sprite-image"
+          type="file"
+          accept="image/png"
+          class="mt-2"
+          on:change={onImageUpload}
+        />
+      </div>
       <div class="w-36">
         <SelectInput
           label="Render Route Page"
@@ -75,7 +170,6 @@
               label: "False",
             },
           ]}
-          onChange={saveRenderChange}
         />
       </div>
     {/if}
