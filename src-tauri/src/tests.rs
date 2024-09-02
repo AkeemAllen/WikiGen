53d7115aab4a75a8b1dac5662cad730d1837f723
Akeem Allen <allenakeem8@gmail.com>
2024-09-02 19:12:35 -0400

    created test for migrations

--- src-tauri/src/tests.rs
+++ src-tauri/src/tests.rs
@@ -1,42 +1,17 @@
 #[cfg(test)]
 pub mod tests {
+    use crate::migrations::run_migrations;
 
-    // #[test]
-    // fn test_create_dir_all() {
-    //     let base_path =
-    //         PathBuf::from("/Users/akeemallen/Library/Application Support/com.wikigen.dev/test");
-    //     let base_path_2 =
-    //         PathBuf::from("/Users/akeemallen/Library/Application Support/com.wikigen.dev/Test");
-
-    //     match std::fs::create_dir_all(&base_path) {
-    //         Ok(_) => {
-    //             println!("Lower case Directory created");
-    //         }
-    //         Err(err) => {
-    //             println!("Error creating lower case directory: {}", err);
-    //         }
-    //     };
-    //     let path_1_exists = base_path.try_exists().unwrap();
-    //     println!("{:?}", path_1_exists);
-
-    //     match std::fs::create_dir_all(&base_path_2) {
-    //         Ok(_) => {
-    //             println!("Higher case Directory created");
-    //         }
-    //         Err(err) => {
-    //             println!("Error creating higher case directory: {}", err);
-    //         }
-    //     };
-
-    //     let path_2_exists = base_path_2.try_exists().unwrap();
-    //     println!("{:?}", path_2_exists);
-    // }
-
-    // #[test]
-    // fn test_generate_pokemon_page(){
-    //     let base_path =
-    //                 PathBuf::from("/Users/akeemallen/Library/Application Support/com.wikigen.dev/test-wiki");
-
-    //     let result = generate_pokemon_page(&base_path, &pokemon);
-    // }
+    #[test]
+    fn test_run_migrations() {
+        let base_path = std::path::PathBuf::from(
+            "/Users/akeemallen/Library/Application Support/com.wikigen.dev",
+        );
+        match tauri::async_runtime::block_on(run_migrations(&base_path)) {
+            Ok(_) => {}
+            Err(err) => {
+                panic!("Failed to run migrations: {}", err);
+            }
+        }
+    }
 }
