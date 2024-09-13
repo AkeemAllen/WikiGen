04d85a45591860284cf29d9b1c66a4300f7ee2c1
Akeem Allen <allenakeem8@gmail.com>
2024-09-13 00:14:23 -0400

    Updated migrations to run on existing wikis

--- src-tauri/src/migrations/mod.rs
+++ src-tauri/src/migrations/mod.rs
@@ -3,14 +3,14 @@ use std::path::PathBuf;
 pub mod db_migrations;
 pub mod file_migrations;
 
-pub async fn run_migrations(base_path: &PathBuf, resource_path: &PathBuf) -> Result<(), String> {
-    match db_migrations::run_db_migrations(base_path).await {
+pub async fn run_migrations(base_path: &PathBuf, resources_path: &PathBuf) -> Result<(), String> {
+    match db_migrations::run_db_migrations(base_path, resources_path).await {
         Ok(_) => {}
         Err(err) => {
             println!("Error running database migrations: {}", err);
         }
     }
-    match file_migrations::run_file_migrations(base_path, resource_path) {
+    match file_migrations::run_file_migrations(base_path, resources_path) {
         Ok(_) => {}
         Err(err) => {
             println!("Error running file migrations: {}", err);
