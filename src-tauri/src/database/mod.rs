9109d71a6834e18bbd6bc06dc1d5649bb17e5cfd
Akeem Allen <allenakeem8@gmail.com>
2024-09-14 17:05:48 -0400

    Created database module to help with database and file calls

---
+++ src-tauri/src/database/mod.rs
@@ -0,0 +1,38 @@
+use std::path::PathBuf;
+
+use sqlx::{migrate::MigrateDatabase, Pool, Sqlite, SqlitePool};
+
+use crate::structs::mkdocs_structs::MKDocsConfig;
+
+pub async fn get_sqlite_connection(
+    sqlite_file_path: std::path::PathBuf,
+) -> Result<Pool<Sqlite>, String> {
+    let sqlite_connection_string = format!("sqlite:{}", sqlite_file_path.to_str().unwrap());
+    if !Sqlite::database_exists(&sqlite_connection_string)
+        .await
+        .unwrap_or(false)
+    {
+        return Err("Database does not exist".to_string());
+    }
+    match SqlitePool::connect(&sqlite_connection_string).await {
+        Ok(conn) => Ok(conn),
+        Err(err) => Err(format!("Failed to connect to database: {}", err)),
+    }
+}
+
+pub fn get_mkdocs_config(file_path: &PathBuf) -> Result<MKDocsConfig, String> {
+    let mkdocs_yaml_file = match std::fs::File::open(file_path) {
+        Ok(file) => file,
+        Err(err) => {
+            return Err(format!("Failed to open mkdocs yaml file: {}", err));
+        }
+    };
+    let config: MKDocsConfig = match serde_yaml::from_reader(mkdocs_yaml_file) {
+        Ok(mkdocs) => mkdocs,
+        Err(err) => {
+            return Err(format!("Failed to parse mkdocs yaml file: {}", err));
+        }
+    };
+
+    return Ok(config);
+}
