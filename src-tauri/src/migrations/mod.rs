f421f8afaa91587343d4d41eced994a9175c5595
Akeem Allen <allenakeem8@gmail.com>
2024-10-12 00:45:48 -0400

    Moved migration check to rust layer

--- src-tauri/src/migrations/mod.rs
+++ src-tauri/src/migrations/mod.rs
@@ -1,7 +1,11 @@
-use std::{collections::HashMap, fs::File, path::PathBuf};
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    path::PathBuf,
+};
 
 use serde::{Deserialize, Serialize};
-use sqlx::Executor;
+use sqlx::{Error, Executor, Pool, Sqlite};
 use tauri::AppHandle;
 
 use crate::{database::get_sqlite_connection, logger};
@@ -30,10 +34,40 @@ pub struct Manifest {
     pub version: String,
 }
 
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Migration {
+    pub name: String,
+    pub app_version: String,
+    pub sql: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct MigrationJson {
+    migrations_present: bool,
+}
+
 #[tauri::command]
-pub async fn run_migrations(app_handle: AppHandle) -> Result<(), String> {
+pub async fn run_migrations(app_handle: AppHandle) -> Result<String, String> {
     // check for migrations folder for current app version. If not present or folder empty, return early.
     let base_path = app_handle.path_resolver().app_data_dir().unwrap();
+    let resources_path = app_handle.path_resolver().resource_dir().unwrap();
+
+    let migrations_file_path = resources_path
+        .join("resources")
+        .join("migrations")
+        .join("migration.json");
+    let migration_file = match File::open(&migrations_file_path) {
+        Ok(file) => file,
+        Err(err) => return Err(format!("Failed to read migrations file: {}", err)),
+    };
+    let mut migration_json: MigrationJson = match serde_json::from_reader(migration_file) {
+        Ok(migration_json) => migration_json,
+        Err(err) => return Err(format!("Failed to parse migrations file: {}", err)),
+    };
+
+    if migration_json.migrations_present == false {
+        return Ok("skipping".to_string());
+    }
 
     let wiki_json_file_path = base_path.join("wikis.json");
     let wikis_file = match File::open(&wiki_json_file_path) {
