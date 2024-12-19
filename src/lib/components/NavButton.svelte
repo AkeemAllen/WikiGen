36b39349bb279d509e4fd70d35132300bcdc0389
Akeem Allen <allenakeem8@gmail.com>
2024-12-19 01:17:30 -0500

    Slight change to Navbutton

--- src/lib/components/NavButton.svelte
+++ src/lib/components/NavButton.svelte
@@ -7,7 +7,7 @@
 <a
   href={route}
   id="sidebar-left"
-  class={`p-1 border-0 w-full hover:cursor-pointer  ${active ? "bg-red-100" : "bg-transparent hover:bg-gray-200"} rounded-md text-sm text-gray-500 space-between`}
+  class={`p-1 border-0 w-full hover:cursor-pointer  ${active ? "bg-red-100" : "bg-transparent hover:bg-zinc-100"} rounded-md text-sm text-gray-500 space-between`}
 >
   <div class="flex items-center gap-3">
     <div class="p-1 rounded-md">
