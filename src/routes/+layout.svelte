12f452a50ee4bfddf64c5325b7d3526bbd9f9e88
Akeem Allen <allenakeem8@gmail.com>
2025-04-03 19:40:25 -0400

    Slight refactor of loading Modals

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -56,6 +56,7 @@
   import { BaseDirectory, writeTextFile } from "@tauri-apps/api/fs";
   import { appDataDir } from "@tauri-apps/api/path";
   import { type } from "@tauri-apps/api/os";
+  import LoadingModal from "$lib/components/modals/LoadingModal.svelte";
 
   initializeStores();
 
@@ -70,6 +71,7 @@
   let deleteWikiModalOpen = false;
   let deployingWiki = false;
   let deployWikiFinalStepsModal = false;
+  let signingIntoGithub = false;
   let osType = "";
 
   let mkdocsFilePath: string = "";
@@ -195,6 +197,7 @@
   }
 
   async function signInToGithub() {
+    signingIntoGithub = true;
     const url = new URL("https://github.com/login/oauth/authorize");
     const params = new URLSearchParams();
     params.append("client_id", "Ov23li9oWejO62cA6Kee");
@@ -205,12 +208,14 @@
     const webview = new WebviewWindow("GithubAccessRequest", {
       url: url.toString(),
       title: "Github Access Request",
+      alwaysOnTop: true,
     });
 
     const unlisten = await webview.listen("loading-token", (event: any) => {
       let data = parseJwt(event.payload.token);
       localStorage.setItem("token", event.payload.token);
       $user = data;
+      signingIntoGithub = false;
 
       webview.close();
     });
@@ -322,44 +327,19 @@
   }
 </script>
 
-<BaseModal bind:open={runningMigrations} close={() => {}} class="w-[30rem]">
-  <div class="flex gap-2 items-center">
-    <svg
-      aria-hidden="true"
-      class="h-5 w-5 animate-spin fill-indigo-600 text-gray-200"
-      viewBox="0 0 100 101"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path
-        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-        fill="currentColor"
-      /><path
-        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-        fill="currentFill"
-      /></svg
-    >
-    <p>Current Upating Database...This might take a while</p>
-  </div>
-</BaseModal>
-<BaseModal bind:open={deployingWiki} close={() => {}} class="w-[30rem]">
-  <div class="flex gap-2 items-center">
-    <svg
-      aria-hidden="true"
-      class="h-5 w-5 animate-spin fill-indigo-600 text-gray-200"
-      viewBox="0 0 100 101"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path
-        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-        fill="currentColor"
-      /><path
-        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-        fill="currentFill"
-      /></svg
-    >
-    <p>Preparing Wiki For Deployment</p>
-  </div>
-</BaseModal>
+<LoadingModal
+  message="Current Upating Database...This might take a while"
+  bind:loading={runningMigrations}
+/>
+<LoadingModal
+  message="Preparing Wiki For Deployment"
+  bind:loading={deployingWiki}
+/>
+<LoadingModal message={updateStatus} bind:loading={updaterModalOpen} />
+<LoadingModal
+  message="Signing Into Github. Webview may take a moment to load."
+  bind:loading={signingIntoGithub}
+/>
 <BaseModal
   bind:open={deployWikiFinalStepsModal}
   close={() => (deployWikiFinalStepsModal = false)}
@@ -406,25 +386,6 @@
         >
       </a>
     </p>
-  </div>
-</BaseModal>
-<BaseModal bind:open={updaterModalOpen} close={() => {}} class="w-[30rem]">
-  <div class="flex gap-2 items-center">
-    <svg
-      aria-hidden="true"
-      class="h-5 w-5 animate-spin fill-indigo-600 text-gray-200"
-      viewBox="0 0 100 101"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path
-        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-        fill="currentColor"
-      /><path
-        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-        fill="currentFill"
-      /></svg
-    >
-    <p>{updateStatus}</p>
   </div>
 </BaseModal>
 
