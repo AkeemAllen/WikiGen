600c201d53fc74368cbdd59fdba3cf3ddc829788
Akeem Allen <allenakeem8@gmail.com>
2025-05-25 05:01:01 -0400

    Added onchange evaluation to Number Input

--- src/lib/components/NumberInput.svelte
+++ src/lib/components/NumberInput.svelte
@@ -8,6 +8,7 @@
     value?: number | null;
     disabled?: boolean;
     class?: string;
+    onchange: Function;
   }
 
   let {
@@ -18,9 +19,9 @@
     max = 2000,
     value = $bindable(0),
     disabled = false,
-    class: className = ""
+    class: className = "",
+    onchange = () => {},
   }: Props = $props();
-
 </script>
 
 <div class={className}>
@@ -29,14 +30,15 @@
   >
   <div class="mt-2">
     <input
-      id={id}
-      disabled={disabled}
+      {id}
+      {disabled}
       type="number"
-      min={min}
-      max={max}
-      placeholder={placeholder}
+      {min}
+      {max}
+      {placeholder}
+      onchange={() => onchange()}
       class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:bg-gray-100 disabled:text-gray-400 sm:text-sm sm:leading-6"
-      bind:value={value}
+      bind:value
     />
   </div>
 </div>
