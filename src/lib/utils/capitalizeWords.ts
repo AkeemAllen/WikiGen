8e62a84aa56103e2c1d578577a4cbb354405b67f
Akeem Allen <allenakeem8@gmail.com>
2025-09-06 01:10:37 -0400

    Small bug fix where function was trying to split null

--- src/lib/utils/capitalizeWords.ts
+++ src/lib/utils/capitalizeWords.ts
@@ -1,4 +1,5 @@
-export default function capitalizeWords(str: string): string {
+export default function capitalizeWords(str: string | null): string {
+  if (str === null) return "";
   return str
     .split(/[-_]/)
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
